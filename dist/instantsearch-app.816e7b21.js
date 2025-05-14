8a1bffb9e96471b8a0aee285371ac2d1b3797d5e
sayed-ansarali-code <78697406+sayed-ansarali-code@users.noreply.github.com>
2025-05-14 15:13:22 +0530

    initial commit

---
+++ dist/instantsearch-app.816e7b21.js
@@ -0,0 +1,43 @@
+const { algoliasearch, instantsearch } = window;
+const searchClient = algoliasearch('JRP80R92DD', '825a3f887a1e21f7297bee0de5a540b7');
+const search = instantsearch({
+    indexName: 'algolia_movie_sample_dataset',
+    searchClient,
+    future: {
+        preserveSharedStateOnUnmount: true
+    }
+});
+search.addWidgets([
+    instantsearch.widgets.searchBox({
+        container: '#searchbox'
+    }),
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            item: (hit, { html, components })=>html`
+        <article>
+          <img src=${hit.poster_path} alt=${hit.title} />
+          <div>
+            <h1>${components.Highlight({
+                    hit,
+                    attribute: 'title'
+                })}</h1>
+            <p>${components.Highlight({
+                    hit,
+                    attribute: 'overview'
+                })}</p>
+          </div>
+        </article>
+      `
+        }
+    }),
+    instantsearch.widgets.configure({
+        hitsPerPage: 8
+    }),
+    instantsearch.widgets.pagination({
+        container: '#pagination'
+    })
+]);
+search.start();
+
+//# sourceMappingURL=instantsearch-app.816e7b21.js.map
