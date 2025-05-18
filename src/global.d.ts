4dba9ffd756680bac9d1f6705c7fa449d4cc95f9
sayed-ansarali-code <78697406+sayed-ansarali-code@users.noreply.github.com>
2025-05-18 10:26:43 +0530

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js/dist/instantsearch.production.min';
+import { Analytics } from "@vercel/analytics/next"
 
 declare global {
   interface Window {
